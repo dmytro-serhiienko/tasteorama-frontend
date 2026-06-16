0bac640c75ffb2fa0afb75eb0e50a6c076864323
AlenaLarzhevska14 <alenna11358@gmail.com>
2026-06-17 00:48:09 +0300

    Resolve merge conflicts

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -14,16 +14,15 @@ import SearchBox from '../SearchBox/SearchBox';
 const Hero = () => {
   return (
     <section className={css.hero}>
-<<<<<<< HEAD
       <div className={css.content}>
-        <h1 className={css.title}>Plan, Cook, and Share Your Flavors</h1>
+        <h1 className={css.title}>
+          Plan, Cook, and
+          <br />
+          Share Your Flavors
+        </h1>
 
         <SearchBox />
       </div>
-=======
-      <h3 className={css.hero}>Hero</h3>
-      {/* <SearchBox /> */}
->>>>>>> main
     </section>
   );
 };
